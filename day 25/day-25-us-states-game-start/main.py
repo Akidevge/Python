11554c624f682618d8a8f54296b0464774f9057a
Akidevge <zurajanai737@gmail.com>
2024-02-13 18:31:09 +0530

    Added Exit path and exporting missing states

--- day 25/day-25-us-states-game-start/main.py
+++ day 25/day-25-us-states-game-start/main.py
@@ -2,33 +2,44 @@ from email.mime import image
 import turtle
 from tkinter import messagebox
 import pandas as pd
-data=pd.read_csv("day 25\\day-25-us-states-game-start\\50_states.csv")
-stateList=data.state.tolist()
-screen=turtle.Screen()
+data = pd.read_csv("day 25\\day-25-us-states-game-start\\50_states.csv")
+stateList = data.state.tolist()
+screen = turtle.Screen()
 screen.title("US States")
-image="day 25\\day-25-us-states-game-start\\blank_states_img.gif"
+image = "day 25\\day-25-us-states-game-start\\blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-gameover=True
-answerList=[]
-while gameover:
-    answerCount=len(answerList)
-    answer=str(screen.textinput(title=f"Guess the state{answerCount}/50",prompt="Name the state")).title()
-    if answer in answerList:
-        t=turtle.Turtle()
+answerList = []
+answerCount = 0
+while answerCount < 50:
+    answerCount = len(answerList)
+    answer = str(screen.textinput(
+        title=f"Guess the state{answerCount}/50", prompt="Name the state")).title()
+    if answer == "Exit":
+        break
+    elif answer in answerList:
+        t = turtle.Turtle()
         messagebox.showinfo(title="Error", message="Enter a New State Name!")
     elif answer not in stateList:
-        t=turtle.Turtle()
+        t = turtle.Turtle()
         messagebox.showinfo(title="Error", message="Enter a Valid State Name!")
+    elif answer == "exit":
+        break
     else:
         answerList.append(answer)
-        state_data=data[data["state"]==answer]
+        state_data = data[data["state"] == answer]
         print(state_data)
-        loc_x=int(data[data.state==answer].x.values[0])
-        loc_y=int(data[data.state==answer].y.values[0])
-        answers=turtle.Turtle()
+        loc_x = int(data[data.state == answer].x.values[0])
+        loc_y = int(data[data.state == answer].y.values[0])
+        answers = turtle.Turtle()
         answers.hideturtle()
         answers.penup()
-        answers.goto(loc_x,loc_y)
+        answers.goto(loc_x, loc_y)
         answers.write(answer)
+missingState = []
+for state in stateList:
+    if state not in answerList:
+        missingState.append(state)
+df = pd.DataFrame(missingState)
+df.to_csv("day 25\\day-25-us-states-game-start\\missingstates.csv")
 screen.exitonclick()
